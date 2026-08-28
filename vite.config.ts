909a3f4764fcb482d550d07290a7c584a5711b58
workcodcentral <work.codcentral@gmail.com>
2026-08-28 12:13:40 +0300

    Use base / in dev, /codcentral-studios/ in production

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/codcentral-studios/',
+  base: process.env.NODE_ENV === 'production' ? '/codcentral-studios/' : '/',
 })
